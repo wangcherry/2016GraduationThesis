3e85cadf1661edd2185f0fde68993117520dc3db
hzwangying1 <hzwangying1@corp.netease.com>
2016-04-09 22:03:14 +0800

    msic:基本完成页面逻辑

--- wangying/mycode/src/scripts/index.js
+++ wangying/mycode/src/scripts/index.js
@@ -104,6 +104,7 @@ $(function() {
     var scanFlag = true;   //true代表处于扫药单状态
     var testFlag = false;   //false代表药单没有通过验证
     var drugNum,prescNum;
+    var prescInfo = $('.js-presc-info');
     var druInfoTable = $('.js-drug-info-table');
     var searchDrugTable = $('.js-search-drug-table');
     var searchDrug = $('#searchDrug');
@@ -132,7 +133,7 @@ $(function() {
 
 scanSucceed();
     //扫描成功后操作
-    function scanSucceed(){
+    function scanSucceed(prescCode){
         var prescCode = '6937748304647';
         if(scanFlag){  //此时是扫描药单
             $.ajax({
@@ -147,9 +148,9 @@ scanSucceed();
                 }
             });
             scanFlag = !scanFlag;
+            prescInfo.show();
         }else{  //此时是扫描药品
             alert("验证药品");
-            alert(prescCode);
         }
     };
     function scanPrescHandler(data,prescCode){
@@ -164,7 +165,7 @@ scanSucceed();
         $('.js-doctors-departments').html(prescData.doctorInfo.doctors_departments);
         prescNum = prescData.checkdrugInfo.length;
         for(var i = 0 ; i < prescNum ; i++){
-            tr += '<tr><td>'+prescData.checkdrugInfo[i].d_name+'</td><td>'+prescData.checkdrugInfo[i].d_id+'</td><td>'+prescData.checkdrugInfo[i].d_specification+'</td><td>'+prescData.checkdrugInfo[i].checkdrug_num+'</td><td>'+prescData.checkdrugInfo[i].d_manufacturer+'</td><td>'+'操作'+'</td><tr>';
+            tr += "<tr><td>"+prescData.checkdrugInfo[i].d_name+"</td><td>"+prescData.checkdrugInfo[i].d_id+"</td><td>"+prescData.checkdrugInfo[i].d_specification+"</td><td>"+prescData.checkdrugInfo[i].checkdrug_num+"</td><td>"+prescData.checkdrugInfo[i].d_manufacturer+"</td><td><a href='javascript:void(0);' class='js-pass'>"+"通过"+"</a> <a href='javascript:void(0);' class='js-search' data-toggle='modal' data-target='#myModal'>"+"查询"+"</a></td><tr>";
         };
         druInfoTable.append(tr);
     }
@@ -212,7 +213,7 @@ scanSucceed();
         var testResultFalg = druInfoTable.find('td').length;
         var testResult = $('.js-test-result')
         if(testResultFalg != 0){
-            testResult.html('还有药品没有通过验证，暂时不可发药！');
+            testResult.html('还有药品未通过验证，暂时不可发药！');
         }else{
             testResult.html('药品验证通过，可发药！');
             testFlag = !testFlag;
@@ -283,8 +284,12 @@ scanSucceed();
             alert('此药单通过了验证，请发药！');
             testResultHandler(testFlag);
         }else{
-            alert('此药单没有通过验证，请勿发药！');
-            testResultHandler(testFlag);
+            if(confirm("此药单没有通过验证，确定发送药单验证结果？")){
+                alert('此药单没有通过验证，请勿发药！');
+                testResultHandler(testFlag);
+            }else{
+                return;
+            }
         };
     });
 });
